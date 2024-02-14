079b5bf38f52d06a5d019a6c5a039f936f101bac
Matthew Quan <maquan@usc.edu>
2024-02-14 13:30:21 -0800

    css styling

--- src/components/frame-group.js
+++ src/components/frame-group.js
@@ -1,34 +1,33 @@
+import styles from "./frame-group.module.css";
+
 const FrameGroup = () => {
-    return (
-      <section className="self-stretch flex flex-col items-end justify-start max-w-full text-left text-41xl text-white font-mada">
-        <header className="self-stretch flex flex-row items-center justify-between py-0 pr-0 pl-5 gap-[20px] text-left text-xl text-black font-inter mq750:hidden">
-          <h2 className="m-0 relative text-inherit font-bold font-inherit whitespace-nowrap">
-            OFFICE OF ACADEMIC RECORDS AND REGISTRAR
-          </h2>
-          <img
-            className="h-[87px] w-[297px] relative object-cover"
-            loading="eager"
-            alt=""
-            src="/screen-shot-20240202-at-1111-1@2x.png"
-          />
-        </header>
-        <div className="self-stretch h-[289px] relative max-w-full">
-          <div className="absolute top-[155px] left-[0px] bg-darkred w-[1440px] flex flex-row items-center justify-start py-7 px-[39px] box-border max-w-full">
-            <div className="h-[134px] w-[1440px] relative bg-darkred hidden max-w-full" />
-            <h1 className="m-0 h-[78px] relative text-inherit font-semibold font-inherit inline-block z-[1] mq450:text-17xl mq750:text-29xl">
-              Final Exam Finder
-            </h1>
-          </div>
-          <img
-            className="absolute top-[0px] left-[0px] w-[1440px] h-[157px] object-cover z-[1]"
-            loading="eager"
-            alt=""
-            src="/bk01-1@2x.png"
-          />
+  return (
+    <section className={styles.frameGroup}>
+      <header className={styles.academicRecordsBlock}>
+        <h2 className={styles.officeOfAcademic}>
+          OFFICE OF ACADEMIC RECORDS AND REGISTRAR
+        </h2>
+        <img
+          className={styles.screenShot20240202At111}
+          loading="eager"
+          alt=""
+          src="/screen-shot-20240202-at-1111-1@2x.png"
+        />
+      </header>
+      <div className={styles.finalExamFinderText}>
+        <div className={styles.rectangleParent}>
+          <div className={styles.frameChild} />
+          <h1 className={styles.finalExamFinder}>Final Exam Finder</h1>
         </div>
-      </section>
-    );
-  };
-  
+        <img
+          className={styles.bk011Icon}
+          loading="eager"
+          alt=""
+          src="/bk01-1@2x.png"
+        />
+      </div>
+    </section>
+  );
+};
+
 export default FrameGroup;
-  
--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -1,13 +1,15 @@
+import Head from "next/head";
 import FrameGroup from "../components/frame-group";
 import InputArea from "../components/input-area";
+import styles from "./index.module.css";
 
 const Desktop = () => {
   return (
-    <div className="w-full relative bg-white overflow-hidden flex flex-col items-center justify-start gap-[33px] tracking-[normal] mq750:gap-[33px]">
+    <div className={styles.desktop1}>
       <FrameGroup />
       <InputArea />
       <img
-        className="self-stretch relative max-w-full overflow-hidden max-h-full object-cover"
+        className={styles.screenShot20240202At112}
         alt=""
         src="/screen-shot-20240202-at-1127-1@2x.png"
       />
